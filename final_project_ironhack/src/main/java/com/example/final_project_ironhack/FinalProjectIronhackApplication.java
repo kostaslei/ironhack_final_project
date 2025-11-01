8431c501930d81e621a02797ec6151e48606856b
Konstantinos Leivaditis <konstantinosleivaditis@Konstantinoss-MacBook-Pro-2.local>
2025-11-01 18:29:55 +0100

    Project init

---
+++ final_project_ironhack/src/main/java/com/example/final_project_ironhack/FinalProjectIronhackApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_project_ironhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectIronhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectIronhackApplication.class, args);
+	}
+
+}
---
+++ final_project_ironhack/src/test/java/com/example/final_project_ironhack/FinalProjectIronhackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_project_ironhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectIronhackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
